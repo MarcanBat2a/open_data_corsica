c088cc9cbd33afe0864b6b8e1a8a7eaae6b97de1
Marcan <marcu-andria.battesti@jellysmack.com>
2022-06-30 11:29:24 +0200

    fix-export-xlsx

--- app/app/api/api_v1/endpoints/care.py
+++ app/app/api/api_v1/endpoints/care.py
@@ -1,5 +1,5 @@
 from app.app.services import commune_services, care_services, borne_recharge_services
-from fastapi import APIRouter
+from fastapi import APIRouter, status, Response
 from fastapi.responses import StreamingResponse
 import io
 import pandas as pd
@@ -19,29 +19,31 @@ def read_cares_communes():
 
 @router.get("/export")
 def export_comment(extension:str):
-
-    if extension.lower()=="csv":
-        stream = io.StringIO()
-        dataframe_result = pd.DataFrame(data=care_services.get_all_care())
-        dataframe_result.to_csv(stream, index = False)
-        response = StreamingResponse(iter([stream.getvalue()]),
-                                media_type="text/csv"
-        )
-        response.headers["Content-Disposition"] = "attachment; filename=data.csv"
-    elif extension.lower()=="xlsx":
-        stream = io.BytesIO()
-        writer = pd.ExcelWriter(stream, engine='xlsxwriter')
-        dataframe_result = pd.DataFrame(data=care_services.get_all_care())
-        dataframe_result.to_excel(writer, index = False)
-        writer.save()
-        xlsx_data = stream.getvalue()
-        response = StreamingResponse(io.BytesIO(xlsx_data), media_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
-        response.headers["Content-Disposition"] = "attachment; filename=filename=data.xls"
-    else:
-        stream = io.StringIO()
-        result = json.dump(care_services.get_all_care_to_dict(), stream)
-        response = StreamingResponse(iter([stream.getvalue()]),
-                                media_type="text/json"
-        )
-        response.headers["Content-Disposition"] = "attachment; filename=data.json"
-    return response
+    try:
+        if extension.lower()=="csv":
+            stream = io.StringIO()
+            dataframe_result = pd.DataFrame(data=care_services.get_all_care())
+            dataframe_result.to_csv(stream, index = False)
+            response = StreamingResponse(iter([stream.getvalue()]),
+                                    media_type="text/csv"
+            )
+            response.headers["Content-Disposition"] = "attachment; filename=data.csv"
+        elif extension.lower()=="xlsx":
+            stream = io.BytesIO()
+            writer = pd.ExcelWriter(stream, engine='xlsxwriter')
+            dataframe_result = pd.DataFrame(data=care_services.get_all_care())
+            dataframe_result.to_excel(writer, index = False)
+            writer.save()
+            xlsx_data = stream.getvalue()
+            response = StreamingResponse(io.BytesIO(xlsx_data), media_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+            response.headers["Content-Disposition"] = "attachment; filename=filename=data.xls"
+        else:
+            stream = io.StringIO()
+            result = json.dump(care_services.get_all_care_to_dict(), stream)
+            response = StreamingResponse(iter([stream.getvalue()]),
+                                    media_type="text/json"
+            )
+            response.headers["Content-Disposition"] = "attachment; filename=data.json"
+        return response
+    except:
+        return Response("Choose csv or xlsx or json", status_code=422)
